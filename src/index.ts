ad557553791e6c39334803854e759fa557a874b2
M-Araumi <3m5soy2@gmail.com>
2026-05-22 22:17:26 +0900

    refactor: コマンド解析処理をparseCommandへ分離

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,11 @@
 import { loadState, saveState } from "./storage.js";
-import type { State, Command } from "./types.js";
-import { validateUpdate } from "./validators/validateUpdate.js";
-import { parseOptions } from "./utils/parseOptions.js";
-import { updateOptionMap } from "./constants/optionMaps.js";
+import type { State } from "./types.js";
+import { parseCommand } from "./utils/parseCommand.js";
 import { executeCommand } from "./services/executeCommand.js";
 
 const argv = process.argv.slice(2);
-const [command, ...rest] = argv;
-let cmd: Command;
 
-switch (command) {
-    case "add":
-        if (!rest[0] || !rest[1]) {
-            throw new Error("titleとmemoTextなし");
-        }
-        cmd = {
-            type: "add",
-            input: {
-                title: rest[0],
-                memoText: rest[1]
-            }
-        };
-        break;
-    case "display":
-        cmd = {
-            type: "display"
-        };
-        break;
-    case "update":
-        const options = parseOptions(rest, updateOptionMap);
-        validateUpdate(options);
-        cmd = {
-            type: "update",
-            input: {
-                  id: Number(options.id),
-                  ...(options.title !== undefined && { title: options.title }),
-                  ...(options.memoText !== undefined && { memoText: options.memoText })
-            }
-        };
-        break;
-    case "del":
-        if (!rest[0]) {
-            throw new Error("削除するメモのIDを入力してください");
-        }
-        cmd = {
-            type: "del",
-            input: {
-                id: Number(rest[0])
-            }
-        };
-        break;
-    case "search":
-        if (!rest[0]){
-            throw new Error("検索するキーワードを入力してください");
-        }
-        cmd = {
-            type: "search",
-            input: {
-                keyword: rest[0]
-            }
-        };
-        break;
-    default:
-        throw new Error("不正コマンド");
-        break;
-}
+const cmd = parseCommand(argv)
 
 let state: State = await loadState();
 
---
+++ src/utils/parseCommand.ts
@@ -0,0 +1,59 @@
+import type { Command } from "../types.js";
+import { parseOptions } from "./parseOptions.js";
+import { validateUpdate } from "../validators/validateUpdate.js";
+import { updateOptionMap } from "../constants/optionMaps.js";
+
+export function parseCommand(argv: string[]): Command {
+    const [command, ...rest] = argv;
+    
+    switch (command) {
+        case "add":
+            if (!rest[0] || !rest[1]) {
+                throw new Error("titleとmemoTextなし");
+            }
+            return {
+                type: "add",
+                input: {
+                    title: rest[0],
+                    memoText: rest[1]
+                }
+            };
+        case "display":
+            return {
+                type: "display"
+            };
+        case "update":
+            const options = parseOptions(rest, updateOptionMap);
+            validateUpdate(options);
+            return {
+                type: "update",
+                input: {
+                      id: Number(options.id),
+                      ...(options.title !== undefined && { title: options.title }),
+                      ...(options.memoText !== undefined && { memoText: options.memoText })
+                }
+            };
+        case "del":
+            if (!rest[0]) {
+                throw new Error("削除するメモのIDを入力してください");
+            }
+            return {
+                type: "del",
+                input: {
+                    id: Number(rest[0])
+                }
+            };
+        case "search":
+            if (!rest[0]){
+                throw new Error("検索するキーワードを入力してください");
+            }
+            return {
+                type: "search",
+                input: {
+                    keyword: rest[0]
+                }
+            };
+        default:
+            throw new Error("不正コマンド");
+    }
+}
